9d736766759e644a400ce8ef4be8be81caeb824c
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-02-17 13:40:02 +0000

    create about page

---
+++ src/app/About/page.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-x-4">
+      <div className=" font-semibold px-5 space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600 text-center">
+          About us
+        </h1>
+        <p>
+          Welcome to our streaming platform! We are a team of movie lovers who
+          are passionate about bringing the best films and TV shows to your
+          screen.
+        </p>
+        <p>
+          Our goal is to provide a unique, hassle-free streaming experience that
+          allows you to discover and watch your favorite movies and shows
+          anytime, anywhere. Our extensive collection includes a wide range of
+          genres, from classic films to the latest blockbusters, as well as
+          popular TV series and documentaries.
+        </p>
+        <p>
+          We believe that movies and TV shows are not just a form of
+          entertainment, but also a reflection of our culture, history, and
+          society. That's why we strive to curate a diverse selection of content
+          that speaks to different audiences and perspectives.
+        </p>
+        <p>
+          At our platform, you can not only watch films but also find detailed
+          information about each title, including ratings, reviews, trailers,
+          and cast and crew information. We also provide personalized
+          recommendations based on your viewing history and preferences, so you
+          can discover new content that you'll love.
+        </p>
+        <p>
+          We are committed to providing the highest quality streaming
+          experience, with fast load times, high-resolution video, and reliable
+          streaming on any device. Our platform is also secure and
+          user-friendly, with easy-to-use navigation and a clean, modern design.
+        </p>
+        <p>
+          Thank you for choosing our streaming platform as your go-to source for
+          entertainment. We hope you enjoy our selection of films and shows, and
+          we look forward to bringing you more great content in the future.
+        </p>
+      </div>
+    </div>
+  );
+}
