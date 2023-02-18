5c46949d82f30de3f6737231c38305640b64f549
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-02-18 15:10:57 +0000

    create detials page for movies

---
+++ src/app/movie/[id]/page.js
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+export default async function page({ params }) {
+  const movieId = params.id;
+  console.log(movieId);
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+  );
+  const movie = await res.json();
+  return (
+    <div className=" w-full">
+      <div
+        className="p-4 flex flex-col md:pt-8  lg:flex-row 
+            items-center content-center max-w-6xl mx-auto md:space-x-6"
+      >
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          width={600}
+          height={500}
+          alt={movie.title}
+          className={
+            'sm:rounded-t-lg  group-hover:opacity-80 transition-opacity duration-200'
+          }
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+        />
+        <div className="p-3">
+          <h2 className="text-lg mb-3 font-semibold">
+            {movie.title || movie.name}
+          </h2>
+          <p className="text-lg mb-3">{movie.overview}</p>
+          <p>
+            <span className="mr-3 font-semibold">Release Date :</span>
+            {movie.release_date || movie.first_air_date}
+          </p>
+
+          <p>
+            <span className="mr-3 font-semibold">Rating :</span>
+            {movie.vote_count}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ export default function Card({ result }) {
       className=" cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg
   sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group"
     >
-      <Link href={`/movie/${result.title}`}>
+      <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
@@ -27,7 +27,7 @@ export default function Card({ result }) {
           <h2 className="truncate text-lg font-bold">
             {result.title || result.name}
           </h2>
-          <p className=" line-clamp-4 text-md">{result.overview}</p>
+          <p className=" line-clamp-3 text-md">{result.overview}</p>
           <p className="flex items-center">
             {result.release_date || result.first_air_date}
             <HiHandThumbUp className="ml-3" />
